8e938e0ac5ed9448b383b87c891d7e920c29db9b
Fabien Boucher <fboucher@redhat.com>
2019-03-24 16:47:03 +0100

    Use search API

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-from github3 import github
+import github3
+import json
+
+def get_repo_info(gh, _repo):
+    try:
+        repo = gh.repository(_repo.owner, _repo.name)
+    except github3.exceptions.ForbiddenError:
+        return
+    info = {
+        'id': repo.id,
+        'name': repo.name,
+        'url': repo.url,
+        'stars': repo.stargazers_count,
+        'html_url': repo.html_url
+    }
+    return info
+
 
 def main():
-    gh = github.GitHub(token='')
-    for _repo in gh.all_repositories(number=10000, per_page=10):
-        repo = gh.repository(_repo.owner, _repo.name)
-        if repo.archived:
-            print("%s skipped as archived" % repo.name)
-            continue
-        if repo.parent:
-            print("%s skipped as forked" % repo.name)
-            continue
-        if repo.stargazers_count <= 500:
-            print("%s skipped as star < 500" % repo.name)
-            continue
-        print("Name: %s, Star: %s, Url: %s" % (
-            repo.name,
-            repo.stargazers_count,
-            repo.html_url,
-            )
-        )
-
+    count = 0
+    gh = github3.github.GitHub(token='')
+    # https://api.github.com/search/repositories?q=stars:>=500+is:public+fork:false+archived:false
+    # See total_count
+    for _repo in gh.search_repositories(
+            query='stars:>50000 is:public fork:false archived:false'):
+        info = get_repo_info(gh, _repo.repository)
+        count += 1
+        with open('data.json', 'a+') as fd:
+            fd.write(json.dumps(info)+'\n')
+            fd.flush()
+        print("%s registered" % count)
+        
 main()
