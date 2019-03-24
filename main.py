b69538c97e4e79336a20d795fe8985c113dff6e0
Fabien Boucher <fboucher@redhat.com>
2019-03-24 08:58:06 +0100

    Add base code

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-#!/bin/env python
+from github3 import github
 
 def main():
-    pass
+    gh = github.GitHub(token='')
+    for _repo in gh.all_repositories(number=10000, per_page=10):
+        repo = gh.repository(_repo.owner, _repo.name)
+        if repo.archived:
+            print("%s skipped as archived" % repo.name)
+            continue
+        if repo.parent:
+            print("%s skipped as forked" % repo.name)
+            continue
+        if repo.stargazers_count <= 500:
+            print("%s skipped as star < 500" % repo.name)
+            continue
+        print("Name: %s, Star: %s, Url: %s" % (
+            repo.name,
+            repo.stargazers_count,
+            repo.html_url,
+            )
+        )
 
 main()
